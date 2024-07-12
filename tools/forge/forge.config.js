10cecdbae392d6ecfaadd72edceedb21133eb843
Clarissa Xavier <clarissaxr@gmail.com>
2024-07-12 16:35:52 -0300

    update maker-zip

---
+++ tools/forge/forge.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const rootDir = process.cwd();
+
+module.exports = {
+  packagerConfig: {
+    name: "ElectronStarter",
+    executableName: "ElectronStarter",
+    icon: path.resolve("assets/icon"),
+    extraResource: ["assets"],
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: "@electron-forge/maker-squirrel",
+      config: {
+        name: "ElectronStarter",
+      },
+    },
+    {
+      name: "@electron-forge/maker-zip",
+      executableName: "ElectronStarter",
+      platforms: ["darwin", "linux", "win32"],
+    },
+    {
+      name: "@electron-forge/maker-deb",
+      executableName: "ElectronStarter",
+      config: {},
+    },
+    {
+      name: "@electron-forge/maker-rpm",
+      executableName: "ElectronStarter",
+      config: {},
+    },
+  ],
+  plugins: [
+    {
+      name: "@electron-forge/plugin-webpack",
+      config: {
+        mainConfig: path.join(rootDir, "./tools/webpack/main.webpack.js"),
+        renderer: {
+          config: path.join(rootDir, "./tools/webpack/renderer.webpack.js"),
+          entryPoints: [
+            {
+              html: "./public/index.html",
+              js: "./src/index.tsx",
+              name: "main_window",
+              preload: {
+                js: "./electron/bridge.ts", //trocar pra payload.ts
+              },
+            },
+          ],
+        },
+      },
+    },
+  ],
+};
